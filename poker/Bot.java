43a44cc052eddf14672a97f1e9a9fafe51d45545
dazete92 <teno3eric@yahoo.com>
2015-10-16 18:40:47 -0700

    added Bot table, haven't tested yet

--- poker/Bot.java
+++ poker/Bot.java
@@ -6,7 +6,27 @@ public class Bot {
    boolean dealerButton;
    boolean bigBlind;
    boolean smallBlind;
-   
+
+   // Unsuited: start with column
+   // Suited: start with row
+   int[][] holeCardValues = new int[][] {
+      { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },	//0
+      { 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0 },  //1
+      { 0, 0, 7, 8, 8, 8, 8, 8, 8, 8, 8, 8, 8, 7, 5 },  //2
+      { 0, 0, 8, 6, 8, 8, 8, 8, 8, 8, 8, 8, 8, 7, 5 },  //3
+      { 0, 0, 8, 8, 5, 8, 8, 8, 8, 8, 8, 8, 8, 6, 5 },  //4
+      { 0, 0, 8, 8, 8, 4, 8, 8, 8, 8, 8, 8, 7, 6, 4 },  //5
+      { 0, 0, 8, 8, 8, 8, 3, 8, 8, 8, 8, 8, 7, 6, 4 },  //6
+      { 0, 0, 8, 8, 8, 8, 8, 2, 8, 8, 8, 7, 6, 5, 4 },  //7
+      { 0, 0, 8, 8, 8, 8, 8, 8, 2, 8, 7, 7, 6, 5, 4 },  //8
+      { 0, 0, 8, 8, 8, 8, 8, 8, 7, 2, 6, 6, 5, 4, 3 },  //9
+      { 0, 0, 8, 8, 8, 8, 8, 7, 6, 6, 1, 5, 5, 4, 3 },  //10
+      { 0, 0, 8, 8, 8, 7, 7, 6, 6, 5, 4, 1, 4, 3, 3 },  //J
+      { 0, 0, 7, 7, 6, 6, 6, 6, 5, 4, 4, 4, 1, 3, 3 },  //Q
+      { 0, 0, 6, 6, 5, 5, 5, 4, 4, 4, 3, 3, 3, 1, 2 },  //K
+      { 0, 0, 5, 4, 4, 4, 4, 3, 3, 3, 2, 2, 2, 1, 1 }   //A	
+   };
+     // 0, 1, 2, 3, 4, 5, 6, 7, 8, 9,10, J, Q, K, A
 
    public Bot(int position, int stack) {
       this.playerPosition = position;
@@ -28,4 +48,19 @@ public class Bot {
 
    }
 
+   public int evalHoleCards(Holecards holecards) {
+     
+      int idx1 = 0, idx2 = 0, temp;
+
+      idx1 = max(holecards.card1.rank, holecards.card2.rank);
+      idx2 = min(holecards.card1.rank, holecards.card2.rank);
+      
+      if (holecards.card1.suit != holecards.card2.suit) {
+	 idx1 = temp;
+	 idx2 = idx1;
+	 idx2 = temp;
+      }
+
+      return holeCardValues[idx1][idx2]
+   }
 }
