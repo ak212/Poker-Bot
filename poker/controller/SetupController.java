49f00d8892680f59732655c4a201d2f99546e5ba
ak212 <akoeppel212@gmail.com>
2015-12-16 23:50:50 -0800

    Forgot to add these

---
+++ poker/controller/SetupController.java
@@ -0,0 +1,94 @@
+package poker.controller;
+
+import javafx.beans.value.ChangeListener;
+import javafx.beans.value.ObservableValue;
+import javafx.fxml.FXML;
+import javafx.scene.control.TextField;
+import poker.Main;
+
+public class SetupController {
+   // Reference to the main application.
+   private Main mainApp;
+   @FXML
+   private TextField handsPerLevelTextField, smallBlindTextField, stackTextField;
+
+   private int hands = 0, smallBlind = 0, stack = 0;
+
+   @FXML
+   private void intialize() {
+      stackTextField.textProperty().addListener(new ChangeListener<String>() {
+         @Override
+         public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+            int temp = 0;
+            
+            try {
+               temp = Integer.parseInt(newValue);
+            }
+            catch (NumberFormatException e) {
+               stackTextField.setText(oldValue);
+            }
+            
+            if (temp >= 1000 && temp <= 1000000) {
+               stack = temp;
+               stackTextField.setText(newValue);
+            }
+            else {
+               stackTextField.setText(oldValue);
+            }
+         }
+      });
+      
+      handsPerLevelTextField.textProperty().addListener(new ChangeListener<String>() {
+         @Override
+         public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+            int temp = 0;
+
+            try {
+               temp = Integer.parseInt(newValue);
+            }
+            catch (NumberFormatException e) {
+               handsPerLevelTextField.setText(oldValue);
+            }
+            
+            if (temp >= 1 && temp <= 100) {
+               hands = temp;
+               handsPerLevelTextField.setText(newValue);
+            }
+            else {
+               handsPerLevelTextField.setText(oldValue);
+            }
+         }
+      });
+
+      smallBlindTextField.textProperty().addListener(new ChangeListener<String>() {
+         @Override
+         public void changed(ObservableValue<? extends String> observable, String oldValue, String newValue) {
+            int temp = 0;
+
+            try {
+               temp = Integer.parseInt(newValue);
+            }
+            catch (NumberFormatException e) {
+               smallBlindTextField.setText(oldValue);
+            }
+
+            if (temp >= stack / 50 && temp <= stack / 10) {
+               smallBlind = temp;
+               smallBlindTextField.setText(newValue);
+            }
+            else {
+               smallBlindTextField.setText(oldValue);
+            }
+         }
+      });
+   }
+
+   @FXML
+   private void handlePlay() {
+      mainApp.showMainView(stack, smallBlind, hands);
+   }
+
+   public void setMainApp(Main mainApp) {
+      this.mainApp = mainApp;
+   }
+}
